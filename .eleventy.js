20dff4f2b631fcea4c705887dd906ceb5cd0a5dc
Peter deHaan <peter@deseloper.com>
2020-03-16 18:25:13 -0700

    Refreshing

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 const del = require("del");
+const _get = require("lodash.get");
 
 del.sync("www");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setQuietMode(true);
 
+  eleventyConfig.addFilter("map", (arr, key) => arr.map(item => _get(item, key)));
   eleventyConfig.addFilter("localeDateString", dateObj => new Date(dateObj).toLocaleDateString());
+  eleventyConfig.addFilter("toFixed", (value, precision=2) => Number(value).toFixed(precision));
 
   // console.log(eleventyConfig);
 
